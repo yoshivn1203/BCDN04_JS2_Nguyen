5ca0c8a790e83c8a6dc09530b1fd6eeedf523bc9
yoshi1203 <yoshivn1203@gmail.com>
2022-07-05 12:42:36 +0700

    finish

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-document.getElementById('btnTinhLuong').onclick = function () {
+document.getElementById('btnTinhLuong').onclick = () => {
   let tienLuong =
     document.getElementById('luongNgay').value *
     document.getElementById('soNgay').value;
@@ -8,7 +8,7 @@ document.getElementById('btnTinhLuong').onclick = function () {
   ).format(tienLuong);
 };
 
-document.getElementById('btnTrungBinh').onclick = function () {
+document.getElementById('btnTrungBinh').onclick = () => {
   let tong =
     Number(document.getElementById('soThuNhat').value) +
     Number(document.getElementById('soThuHai').value) +
@@ -19,13 +19,13 @@ document.getElementById('btnTrungBinh').onclick = function () {
   document.getElementById('trungBinh').innerHTML = tong / 5;
 };
 
-document.getElementById('btnQuyDoi').onclick = function () {
+document.getElementById('btnQuyDoi').onclick = () => {
   let tienUsd = document.getElementById('usd').value;
   tienViet = new Intl.NumberFormat('vn-VN').format(23500 * tienUsd);
   document.getElementById('quyDoi').innerHTML = tienViet;
 };
 
-document.getElementById('btnTinh').onclick = function () {
+document.getElementById('btnTinh').onclick = () => {
   let chieuRong = document.getElementById('chieuRong').value;
   chieuCao = document.getElementById('chieuCao').value;
 
@@ -37,7 +37,7 @@ document.getElementById('btnTinh').onclick = function () {
   ).innerHTML = `Diện tích: ${dienTich}; Chu vi: ${chuVi}`;
 };
 
-document.getElementById('btnTinhTong').onclick = function () {
+document.getElementById('btnTinhTong').onclick = () => {
   let number = document.getElementById('number').value,
     hangChuc = Math.floor(number / 10),
     hangDonvi = number % 10;
